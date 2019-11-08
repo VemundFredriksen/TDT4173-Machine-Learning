acdf86329bcfd596dfbac4e25cb016676bd95a81
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2019-11-08 19:56:54 +0100

    Task 1 and 2 finnished

--- Assignment 3/knn.py
+++ Assignment 3/knn.py
@@ -19,7 +19,17 @@ def load_data(filename):
 
 #Simple euclidian distnace function
 def euclidian_distance(e1, e2):
-    return math.sqrt((e1[0] - e2[0])**2 + (e1[1] - e2[1])**2 + (e1[2] - e2[2])**2 + (e1[3] - e2[3])**2)
+    distance = 0
+    for i in range(len(e1) - 1):
+        distance += (e1[i] - e2[i])**2
+    return math.sqrt(distance)
+
+def average(nearest):
+    aggregate = 0
+    for n in nearest:
+        aggregate += n[3]
+
+    return aggregate/len(nearest)
 
 #KNN With Voting
 def knn(neighbours, value, k = 10):
@@ -34,18 +44,20 @@ def knn(neighbours, value, k = 10):
         distance = euclidian_distance(n, value)
         if(distance < max(distances)):
             i = distances.index(max(distances))
-            classes[i] = n[4]
+            classes[i] = n[len(n) - 1]
             distances[i] = distance
             _neigbours[i] = n
     
-    type = vote(classes, distance, _neigbours)
-    
-    #Returns the most common
-    return type, _neigbours
+    return _neigbours
 
-def vote(classes, distances, k_nearest):
+def vote(k_nearest):
     #Finds the most common among k nearest
     types = {}
+
+    classes = []
+    for k in k_nearest:
+        classes.append(k[len(k) - 1])
+
     for c in classes:
         if(not c in types.keys()):
             types[c] = 1
@@ -59,16 +71,31 @@ def vote(classes, distances, k_nearest):
             
     return type
 
-def main():
-    data = load_data("./data/knn_classification.csv")
+def regression(nearest):
+    return average(nearest)
 
-    #Task 1
+
+def main():
+    classification_data = load_data("./data/knn_classification.csv")
+    regression_data = load_data("./data/knn_regression.csv")
+
+    # #Task 1
     print("\n\nFetching element 124 and performing classification...")
-    d1 = data[123]
-    (type, neighbours) = knn(data, d1)
-    print("Element {} is predicted to be of class {}".format(124, type))
+    d1 = classification_data[123]
+    neighbours = knn(classification_data, d1)
     print("10 nearest neighbours in random order:")
     for n in neighbours:
         print(n)
+    
+    print("Classification by vote shows {}".format(vote(neighbours)))
+
+    #Task 2
+    print("\n\nFetching element 124 and performing regression...")
+    d1 = regression_data[123]
+    neighbours = knn(regression_data, d1)
+    print("10 nearest neighbours in random order:")
+    for n in neighbours:
+        print(n)
+    print("Regression by average shows: {}".format(regression(neighbours)))
     
 main()
